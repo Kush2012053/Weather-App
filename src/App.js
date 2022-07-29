88d3752ede2a5130fb7a867526ae0e2e810a8d82
Kush <Kush2012053@akgec.ac.in>
2022-07-28 22:27:17 -0700

    three

--- src/App.js
+++ src/App.js
@@ -2,31 +2,32 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 const App = () => {
-  const [inputName, setInputName] = useState("");
+  const [inputName, setInputName] = useState();
   const [name, setName] = useState("Agra");
-  const [weather, setWeather] = useState({
-    main: "",
-    description: "",
-    icon: "",
-    temp: "",
-    temp_min: "",
-    temp_max: "",
-  });
+  const [weather, setWeather] = useState();
+  const [display, setDisplay] = useState();
   useEffect(() => {
-    const everyRender = async () => {
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=542ad7a41a703c85efd8cac98393e6d8`;
-      const request = axios.get(url);
-      const response = await request;
-      console.log(response);
-      setWeather({
-        main: response.data.weather[0].main,
-        description: response.data.weather[0].description,
-        icon: response.data.weather[0].icon,
-        temp: (response.data.main.temp - 273.15).toFixed(2),
-        temp_min: (response.data.main.temp_min - 273.15).toFixed(2),
-        temp_max: (response.data.main.temp_max - 273.15).toFixed(2),
-      });
-    };
+    async function everyRender() {
+      try {
+        await axios
+          .post(
+            `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=542ad7a41a703c85efd8cac98393e6d8`
+          )
+          .then(function (response) {
+            setWeather({
+              main: response.data.weather[0].main,
+              description: response.data.weather[0].description,
+              icon: response.data.weather[0].icon,
+              temp: (response.data.main.temp - 273.15).toFixed(2),
+              temp_min: (response.data.main.temp_min - 273.15).toFixed(2),
+              temp_max: (response.data.main.temp_max - 273.15).toFixed(2),
+            });
+            setDisplay(true);
+          });
+      } catch (error) {
+        setDisplay(false);
+      }
+    }
     everyRender();
   }, [name]);
 
@@ -43,7 +44,8 @@ const App = () => {
           <div className="outer-input">
             <input
               type="text"
-              placeholder="Enter a city name"
+              value={inputName}
+              placeholder="Enter a Location"
               className="main-input"
               onChange={changeInput}
             />
@@ -56,33 +58,46 @@ const App = () => {
               onClick={getData}
             />
           </div>
-          <h2 className="gap zero">{name}</h2>
-          <h4 className="gap zero">
-            {weather.main}
-            <br />
-            {weather.description}
-          </h4>
-          <div className="outer-img">
-            <div className="inner-img">
-              <img
-                src={`https://openweathermap.org/img/wn/${weather.icon}@4x.png`}
-                alt="Weather Icon"
-                height="100%"
-                width="100%"
-              />
+          {display ? (
+            <div>
+              <h2 className="gap zero">{name}</h2>
+              <h4 className="gap zero">
+                {weather.main}
+                <br />
+                {weather.description}
+              </h4>
+              <div className="outer-img">
+                <div className="inner-img">
+                  <img
+                    src={`https://openweathermap.org/img/wn/${weather.icon}@4x.png`}
+                    alt="Weather Icon"
+                    height="100%"
+                    width="100%"
+                  />
+                </div>
+              </div>
+              <h1 className="zero">{weather.temp} &#8451;</h1>
+              <div className="overall">
+                <div className="common min">
+                  <h4 className="zero">min</h4>
+                  <h3 className="zero">{weather.temp_min} &#8451;</h3>
+                </div>
+                <div className="common max">
+                  <h4 className="zero">max</h4>
+                  <h3 className="zero">{weather.temp_max} &#8451;</h3>
+                </div>
+              </div>
             </div>
-          </div>
-          <h1 className="zero">{weather.temp} &#8451;</h1>
-          <div className="overall">
-            <div className="min">
-              <h4 className="zero">min</h4>
-              <h3 className="zero">{weather.temp_min} &#8451;</h3>
+          ) : (
+            <div className="error">
+              <div>
+                <b>Error</b>
+                <br />
+                <br />
+                The location is invalid. Enter a valid location.
+              </div>
             </div>
-            <div className="max">
-              <h4 className="zero">max</h4>
-              <h3 className="zero">{weather.temp_max} &#8451;</h3>
-            </div>
-          </div>
+          )}
         </div>
       </div>
     </>
